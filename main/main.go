829249509b9f7ef467d3b4af11c631ef4a04b770
raomx <30790578+raomx@users.noreply.github.com>
2020-01-12 13:09:58 +0800

    add sync.Mutex  and Factory close
    to fix memory leak

--- main/main.go
+++ main/main.go
@@ -3,25 +3,34 @@ package main
 import (
 	"fmt"
 	"jobqueue"
+	"sync"
 	"time"
 )
 
 type Sleep struct {
 	Id int
 }
+var count = 0
+var mu =  sync.Mutex{}
 
 func (s *Sleep)Todo() error {
-	time.Sleep(10 * time.Millisecond)
+	mu.Lock()
+	time.Sleep(10 * time.Microsecond)
 	fmt.Printf("ID %v wake up\n", s.Id)
+	count++
+	mu.Unlock()
 	return nil
 }
 
 func main()  {
 	f := jobqueue.NewFactory(100)
 	f.Run()
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 100000; i++ {
 		var sleep jobqueue.Job = &Sleep{i}
 		f.SetAJob(&sleep)
 	}
+
 	f.Wait()
+	f.Close()
+	fmt.Println(count)
 }
