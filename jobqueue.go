399d8cd9e4df0a46e967b8e10262386e1259c5f6
raomx <30790578+raomx@users.noreply.github.com>
2020-01-12 13:07:07 +0800

    fix memory leak
    Add close chan to fix memory leak

--- jobqueue.go
+++ jobqueue.go
@@ -13,13 +13,15 @@ type Worker struct {
 	workerPool chan chan *Job
 	jobChannel chan *Job
 	doneChan   chan bool
+	closeChan chan struct{}
 }
 
-func newWorker(workerPool chan chan *Job, done chan bool) *Worker {
+func newWorker(workerPool chan chan *Job, doneChan chan bool, closeChan chan struct{}) *Worker {
 	return &Worker{
 		workerPool: workerPool,
 		jobChannel: make(chan *Job),
-		doneChan:   done,
+		doneChan:   doneChan,
+		closeChan:	closeChan,
 	}
 }
 
@@ -34,6 +36,8 @@ func (w *Worker) start()  {
 					log.Printf("%v Todo : %v", job, err)
 				}
 				w.doneChan <- true
+			case <- w.closeChan:
+				return
 			}
 		}
 	}()
@@ -41,10 +45,11 @@ func (w *Worker) start()  {
 
 
 type Factory struct {
-	workerPool chan chan* Job
-	jobQueue   chan* Job
-	doneChan   chan bool
-	maxWorkers int
+	workerPool	chan chan* Job
+	jobQueue	chan* Job
+	doneChan	chan bool
+	closeChan	chan struct{}
+	maxWorkers	int
 	sync.WaitGroup
 }
 
@@ -52,21 +57,27 @@ func NewFactory(maxWorkers int) *Factory {
 	workerPool := make(chan chan* Job, maxWorkers)
 	jobQueue := make(chan* Job)
 	doneChan := make(chan bool)
+	closeChan := make(chan struct{})
 	return &Factory{
 		workerPool: workerPool,
 		jobQueue:   jobQueue,
 		doneChan:   doneChan,
 		maxWorkers: maxWorkers,
+		closeChan:	closeChan,
 	}
 }
 
-func (f *Factory)Run()  {
+func (f *Factory) Run()  {
 	for i := 0; i < f.maxWorkers; i++ {
-		worker := newWorker(f.workerPool, f.doneChan)
+		worker := newWorker(f.workerPool, f.doneChan, f.closeChan)
 		worker.start()
 	}
 
 	go f.dispatch()
+}
+
+func (f *Factory) Close() {
+	close(f.closeChan)
 }
 
 func (f *Factory) dispatch() {
@@ -79,6 +90,8 @@ func (f *Factory) dispatch() {
 			}(job)
 		case <-f.doneChan:
 			f.Done()
+		case <- f.closeChan:
+			return
 		}
 	}
 }
